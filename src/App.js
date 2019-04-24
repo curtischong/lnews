84dd9dd33d1be93072ab289952e3d7bef8a305c5
Curtis Chong <curtischong5@gmail.com>
2019-04-24 18:20:10 -0400

    eat class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import CardLineGraph from './cards/Card'
-
-
-
+import {CardLineGraph, CardText} from './cards/Card'
+import axios from 'axios';
 
 class App extends React.Component{
   constructor(props){
     super(props);
 
-    this.state={
+    this.state = {
       cchartData: {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [
@@ -21,20 +18,43 @@ class App extends React.Component{
             data: [0, 10, 5, 2, 20, 30, 45]
           }
         ]
-      }
+      },
+      cards: []
     }
+    for(let i = 0;i < 4;i++){
+      this.state.cards.push(
+        <CardLineGraph
+          title = "Heartrate"
+          chartData = {this.state.cchartData}
+          key={i}
+        />
+      )
+    }
+      this.state.cards.push(
+        <CardText
+        title="Eat!"
+        desc="Eggs ham, maybe? bread? idk you haven't eaten yet man."
+        img = "images/food.png"
+        key={new Date().getDate()}
+        />
+      )
   }
 
-
+  componentDidMount(){
+    axios.get(`http://localhost:5000/get_card`)
+    .then(res => {
+      console.log(res)
+    });
+  }
 
   render () {
     return (
       <div>
         <div className="App">
           <div className="padding"></div>
-          <CardLineGraph
-            chartData = {this.state.cchartData}
-          />
+            <div className="newsFeed">
+              {this.state.cards}
+          </div>
         </div>
       </div>
     );
--- src/cards/Card.js
+++ src/cards/Card.js
@@ -1,23 +1,14 @@
 import React from 'react';
 
 import './Card.css';
-import { Line } from "react-chartjs-2";
+import { Line,Pie } from "react-chartjs-2";
 
 
 class CardLineGraph extends React.Component{
-
-/*  constructor(props){
-    super(props);
-
-    this.state={
-
-    }
-  }*/
-
   render () {
     return (
       <div className="card--lineGraph">
-        <h3>Heartrate</h3>
+        <h3>{this.props.title}</h3>
         <Line className="chart--lineGraph"
           data={this.props.chartData}
         />
@@ -26,4 +17,47 @@ class CardLineGraph extends React.Component{
   }
 };
 
-export default CardLineGraph;
+class CardPieGraph extends React.Component{
+  render () {
+    return (
+      <div className="card--pieGraph">
+        <h3>{this.props.title}</h3>
+        <Pie className="chart--pieGraph"
+          data={this.props.chartData}
+        />
+      </div>
+    );
+  }
+};
+
+//TODO: there has to be a better way to handle these alt images. A config somewhere?
+class CardText extends React.Component{
+  render () {
+    return (
+      <div className="card--text">
+        <img className="img--card--small" src={this.props.img} alt="images/thinking.png"></img>
+        <div className="card--text__content">
+          <h3 className="card--text__content__header">{this.props.title}</h3>
+          <p className="card--text__content__desc"> {this.props.desc}</p>
+        </div>
+      </div>
+    );
+  }
+};
+
+class NoCards extends React.Component{
+  render(){
+    return(
+      <div class = "card--noCards">
+        <p> Damn you scroll fast :) Live a good life! </p>
+      </div>
+    )
+  }
+};
+
+export {
+  CardLineGraph,
+  CardPieGraph,
+  CardText,
+  NoCards
+}
