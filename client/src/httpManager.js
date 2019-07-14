9adf72d74871d94e53d495286ee98a78448e79db
Curtis Chong <curtischong5@gmail.com>
2019-07-14 16:56:56 -0400

    found working 'get' request

--- client/src/httpManager.js
+++ client/src/httpManager.js
@@ -1,21 +1,40 @@
 import axios from 'axios';
 import {getLizzieServerIP, getLNewsServerIP} from './config'
 
-const getCards = () => {
-  return axios.get(getLizzieServerIP() + "get_cards_and_panels") // This returns a promise!
+const headers = {
+  'Content-Type': 'application/json',
+}
+
+// The reason why I had so much trouble with get is because there is no standard to send data through a body via a get request
+// Post does allow data to be sent in the body
+const getCards = (getCardsAndPanelsObj) => {
+  //return axios.get(getLizzieServerIP() + "get_cards_and_panels", {params: getCardsAndPanelsObj}, {headers: headers}) // This returns a promise!
+  /*return axios({
+    method: 'get',
+    url: getLizzieServerIP() + "get_cards_and_panels",
+    data: JSON.stringify(getCardsAndPanelsObj),
+    headers: headers
+  }) // This returns a promise!*/
+  /*
+  return axios.get(getLizzieServerIP() + "get_cards_and_panels",{
+    params : JSON.stringify(getCardsAndPanelsObj),
+    headers: headers
+  });*/
+  return axios({
+    method: 'post',
+    url: getLizzieServerIP() + "get_cards_and_panels",
+    data: getCardsAndPanelsObj,
+    headers: headers
+  }) // This returns a promise!*/
 };
 
 const sendEmotionEval = (evalObj) => {
-  console.log(evalObj);
   axios.post(getLizzieServerIP() + "upload_emotion_evaluation", JSON.stringify(evalObj))
   .then(res => {
     console.log(res);
   });
 };
 
-var headers = {
-  'Content-Type': 'application/json',
-}
 
 const dismissPanel = (timePlaced) => {
   //dismissPanel
